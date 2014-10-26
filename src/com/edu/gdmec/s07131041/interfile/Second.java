171b40e2ceadc30cc4ddde967500b57e3cdd4339
gdmec07131041 <445626616@qq.com>
2014-10-26 20:06:54 +0800

    homework

---
+++ src/com/edu/gdmec/s07131041/interfile/Second.java
@@ -0,0 +1,16 @@
+package com.edu.gdmec.s07131041.interfile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Second extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub 
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.second);
+	}
+	
+
+}
